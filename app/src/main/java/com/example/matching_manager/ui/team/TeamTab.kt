f1eaaff9be4e67f9cb0e0432949a7964948e90e8
yeonghyeon <agilecatch@gmail.com>
2023-10-12 11:32:39 +0900

    [Feat]:Fragment in Viewpager
    - 프래그먼트안에 뷰페이저 연결을 하였습니다.
    - TeamFragment 안의 ViewPager 스크롤 중첩을 제한 하였습니다.

---
+++ app/src/main/java/com/example/matching_manager/ui/team/TeamTab.kt
@@ -0,0 +1,8 @@
+package com.example.matching_manager.ui.team
+
+import androidx.fragment.app.Fragment
+
+data class TeamTab(
+    val fragment: Fragment,//fragment
+    val title: Int,//탭에 대한 제목
+)
---
+++ app/src/main/java/com/example/matching_manager/ui/team/menu/MyTeamFragment.kt
@@ -0,0 +1,42 @@
+package com.example.matching_manager.ui.team.menu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.matching_manager.databinding.MyTeamFragmentBinding
+import com.example.matching_manager.databinding.TeamFragmentBinding
+
+class MyTeamFragment : Fragment() {
+    private var _binding: MyTeamFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance() = MyTeamFragment()
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = MyTeamFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+    }
+
+    private fun initView() = with(binding) {
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
