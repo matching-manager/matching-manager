4fcf3b8a4b042903667b0d31f00f253993d0c31a
mwkimm <94061061+mwkimm@users.noreply.github.com>
2023-10-13 11:43:34 +0900

    pull 당기기전 저장

--- app/src/main/java/com/example/matching_manager/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/matching_manager/ui/home/HomeFragment.kt
@@ -37,8 +37,8 @@ class HomeFragment : Fragment() {
 
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 }
