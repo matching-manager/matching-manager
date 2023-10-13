307f708544668e97877a2b719b56db8dbe421aef
hjdevelop <hyunjun6133@naver.com>
2023-10-13 11:15:34 +0900

    [Feat] : Match Fragment의 RecyclerView List Adapter 적용

---
+++ app/src/main/java/com/example/matching_manager/ui/match/MatchAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.matching_manager.ui.match
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.matching_manager.databinding.MatchItemBinding
+
+class MatchAdapter (private val onItemClick: (MatchData) -> Unit) : ListAdapter<MatchData, MatchAdapter.ViewHolder> (
+    object :DiffUtil.ItemCallback<MatchData>() {
+        override fun areItemsTheSame(oldItem: MatchData, newItem: MatchData): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: MatchData, newItem: MatchData): Boolean {
+            return oldItem == newItem
+        }
+    }
+        ){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(MatchItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item, onItemClick)
+    }
+
+    class ViewHolder(private val binding: MatchItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item : MatchData, onItemClick: (MatchData) -> Unit) = with(binding) {
+            ivTeam.setImageResource(item.teamImage)
+            tvType.text = item.type
+            tvDetail.text = item.detail
+            tvViewCount.text = item.viewCount.toString()
+            tvChatCount.text = item.chatCount.toString()
+            tvSchedule.text = item.schedule
+            tvPlace.text = item.place
+
+            itemView.setOnClickListener {
+                onItemClick(item)
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/matching_manager/ui/match/MatchFragment.kt
+++ app/src/main/java/com/example/matching_manager/ui/match/MatchFragment.kt
@@ -1,10 +1,14 @@
 package com.example.matching_manager.ui.match
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.matching_manager.R
 import com.example.matching_manager.databinding.MatchFragmentBinding
 import com.example.matching_manager.databinding.SignInFragmentBinding
@@ -14,8 +18,11 @@ class MatchFragment : Fragment() {
     private var _binding: MatchFragmentBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+
     companion object {
         fun newInstance() = MatchFragment()
+        const val OBJECT_DATA = "item_object"
     }
 
     override fun onCreateView(
@@ -34,11 +41,44 @@ class MatchFragment : Fragment() {
     }
 
     private fun initView() = with(binding) {
+        val dummyData = mutableListOf<MatchData>()
+        dummyData.add(MatchData(R.drawable.sonny, "팀 매칭", "11:11 남성", 1, 0, "11/2 (목) 오후 8시", "경기도 안양시 평촌중앙공원 축구장"))
+        dummyData.add(MatchData(R.drawable.sonny, "팀 매칭", "11:11 남성", 1, 0, "11/2 (목) 오후 8시", "경기도 안양시 평촌중앙공원 축구장"))
+        dummyData.add(MatchData(R.drawable.sonny, "팀 매칭", "11:11 남성", 1, 0, "11/2 (목) 오후 8시", "경기도 안양시 평촌중앙공원 축구장"))
+        dummyData.add(MatchData(R.drawable.sonny, "팀 매칭", "11:11 남성", 1, 0, "11/2 (목) 오후 8시", "경기도 안양시 평촌중앙공원 축구장"))
+        dummyData.add(MatchData(R.drawable.sonny, "팀 매칭", "11:11 남성", 1, 0, "11/2 (목) 오후 8시", "경기도 안양시 평촌중앙공원 축구장"))
+        dummyData.add(MatchData(R.drawable.sonny, "팀 매칭", "11:11 남성", 1, 0, "11/2 (목) 오후 8시", "경기도 안양시 평촌중앙공원 축구장"))
+        dummyData.add(MatchData(R.drawable.sonny, "팀 매칭", "11:11 남성", 1, 0, "11/2 (목) 오후 8시", "경기도 안양시 평촌중앙공원 축구장"))
+        dummyData.add(MatchData(R.drawable.sonny, "팀 매칭", "11:11 남성", 1, 0, "11/2 (목) 오후 8시", "경기도 안양시 평촌중앙공원 축구장"))
+        dummyData.add(MatchData(R.drawable.sonny, "팀 매칭", "11:11 남성", 1, 0, "11/2 (목) 오후 8시", "경기도 안양시 평촌중앙공원 축구장"))
+        dummyData.add(MatchData(R.drawable.sonny, "팀 매칭", "11:11 남성", 1, 0, "11/2 (목) 오후 8시", "경기도 안양시 평촌중앙공원 축구장"))
+        dummyData.add(MatchData(R.drawable.sonny, "팀 매칭", "11:11 남성", 1, 0, "11/2 (목) 오후 8시", "경기도 안양시 평촌중앙공원 축구장"))
+        dummyData.add(MatchData(R.drawable.sonny, "팀 매칭", "11:11 남성", 1, 0, "11/2 (목) 오후 8시", "경기도 안양시 평촌중앙공원 축구장"))
+        dummyData.add(MatchData(R.drawable.sonny, "팀 매칭", "11:11 남성", 1, 0, "11/2 (목) 오후 8시", "경기도 안양시 평촌중앙공원 축구장"))
+        dummyData.add(MatchData(R.drawable.sonny, "팀 매칭", "11:11 남성", 1, 0, "11/2 (목) 오후 8시", "경기도 안양시 평촌중앙공원 축구장"))
 
+
+        val adapter = MatchAdapter {item ->
+            val intent = Intent(requireContext(), MatchDetailActivity::class.java)
+            intent.putExtra(OBJECT_DATA, item)
+            startActivity(intent)
+        }
+        rv.adapter = adapter
+
+        rv.layoutManager = LinearLayoutManager(requireContext())
+
+        adapter.submitList(dummyData)
+
+        btnCategory.setOnClickListener {
+            val matchCategory = MatchCategory()
+
+            val fragmentManager = requireActivity().supportFragmentManager
+            matchCategory.show(fragmentManager, matchCategory.tag)
+        }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 }
