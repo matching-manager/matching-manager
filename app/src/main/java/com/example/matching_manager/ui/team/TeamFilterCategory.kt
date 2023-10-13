f25f83c3042bf4a84c66135d0f5d127973f51cd4
yeonghyeon <agilecatch@gmail.com>
2023-10-13 22:45:52 +0900

    [Design] : filter
    - 필터 카테고리 구현

--- app/src/main/java/com/example/matching_manager/ui/team/TeamFilterCategory.kt
+++ app/src/main/java/com/example/matching_manager/ui/team/TeamFilterCategory.kt
@@ -7,16 +7,12 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import com.example.matching_manager.R
 import com.example.matching_manager.databinding.TeamFilterCategoryBinding
-import com.example.matching_manager.ui.team.mdoel.GameModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class TeamFilterCategory : BottomSheetDialogFragment() {
 
     private var _binding: TeamFilterCategoryBinding? = null
     private val binding get() = _binding!!
-    private val gameSpinnerList = ArrayList<GameModel>()
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,28 +23,36 @@ class TeamFilterCategory : BottomSheetDialogFragment() {
 
         _binding = TeamFilterCategoryBinding.inflate(inflater, container, false)
 
-        return binding.root
+        setUpSpinner()
         initView()
+        return binding.root
+    }
+
+    private fun setUpSpinner()= with(binding) {
+        // 스피너에 데이터 연결
+        val gameAdapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.game_array,
+            android.R.layout.simple_spinner_item
+        )
+        gameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        gameSpinner.adapter = gameAdapter
+
+
+        val areaAdapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.area_array,
+            android.R.layout.simple_spinner_item
+        )
+        areaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        citySpinner.adapter = areaAdapter
+
+
+
     }
 
     private fun initView()= with(binding) {
-    // 농구, 축구, 풋살을 추가
-        gameSpinnerList.add(GameModel("풋살"))
-        gameSpinnerList.add(GameModel("축구"))
-        gameSpinnerList.add(GameModel("농구"))
-        gameSpinnerList.add(GameModel("배드민턴"))
-        gameSpinnerList.add(GameModel("볼링"))
 
-        val arraySpinnerUpper = gameSpinnerList.map { it.gametitle }.toTypedArray()
-
-        // 스피너에 데이터 연결
-        val adapter = ArrayAdapter(
-            requireContext(),
-            android.R.layout.simple_spinner_item,
-            arraySpinnerUpper
-        )
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        gameSpinner.adapter = adapter
     }
 
     override fun onDestroyView() {
