6572752205d74035d0676aa071463a42ed853b02
추민수 <devsoftychoo@gmail.com>
2023-10-13 11:35:05 +0900

    [Feat] : RecyclerView Adapter
    1. RecyclerView Adapter 구현완료
    2. ViewModel LiveData와 연결

--- app/src/main/java/com/example/matching_manager/ui/home/arena/ArenaActivity.kt
+++ app/src/main/java/com/example/matching_manager/ui/home/arena/ArenaActivity.kt
@@ -3,6 +3,8 @@ package com.example.matching_manager.ui.home.arena
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
 import com.example.matching_manager.databinding.ArenaActivityBinding
 
 class ArenaActivity : AppCompatActivity() {
@@ -10,6 +12,14 @@ class ArenaActivity : AppCompatActivity() {
     private val binding by lazy { ArenaActivityBinding.inflate(layoutInflater) }
 
     private val viewModel : ArenaViewModel by viewModels()
+
+    private val listAdapter : ArenaListAdapter by lazy {
+        ArenaListAdapter(
+            onClick = { item ->
+                // Click Evnet
+            }
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +30,12 @@ class ArenaActivity : AppCompatActivity() {
     }
 
     private fun initView() = with(binding){
+        rvArena.adapter = listAdapter
     }
 
     private fun initModel() = with(viewModel){
-
-
+        list.observe(this@ArenaActivity, Observer {
+            listAdapter.submitList(it)
+        })
     }
 }
--- app/src/main/java/com/example/matching_manager/ui/home/arena/ArenaModel.kt
+++ app/src/main/java/com/example/matching_manager/ui/home/arena/ArenaModel.kt
@@ -1,11 +1,15 @@
 package com.example.matching_manager.ui.home.arena
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class ArenaModel(
-    private val placeName: String?,
-    private val phone: String?,
-    private val addressName: String?, // 지번 주소
-    private val roadAddressName: String?, // 도로명 주소
-    private val placeUrl: String?, // 장소 상세페이지 URL
+    val placeName: String?,
+    val phone: String?,
+    val addressName: String?, // 지번 주소
+    val roadAddressName: String?, // 도로명 주소
+    val placeUrl: String?, // 장소 상세페이지 URL
     // 중심좌표까지의 거리 (단, x,y 파라미터를 준 경우에만 존재) (단위 meter)
     val distance: String?,
-)
+) : Parcelable
