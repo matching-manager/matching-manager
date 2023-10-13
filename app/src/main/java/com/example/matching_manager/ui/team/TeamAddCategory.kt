6a8f8dacacabda48da814a06751dad0eb2d43187
yeonghyeon <agilecatch@gmail.com>
2023-10-13 16:02:53 +0900

    [Design] : TeamFragment
    - 글 추가버튼 클릭시 BottomSheetDialogFragment 구현

---
+++ app/src/main/java/com/example/matching_manager/ui/team/TeamAddCategory.kt
@@ -0,0 +1,32 @@
+package com.example.matching_manager.ui.team
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.matching_manager.databinding.MatchCategoryBinding
+import com.example.matching_manager.databinding.TeamAddCategoryBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class TeamAddCategory : BottomSheetDialogFragment() {
+
+    private var _binding: TeamAddCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        _binding = TeamAddCategoryBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
