dba49ad7b62e07887b7268587bdcac30d89b5b33
hjdevelop <hyunjun6133@naver.com>
2023-10-17 02:06:37 +0900

    [FIX] : updateList적용 및 detailWriting 인텐트 생성

--- app/src/main/java/com/example/matching_manager/ui/match/MatchFragment.kt
+++ app/src/main/java/com/example/matching_manager/ui/match/MatchFragment.kt
@@ -8,17 +8,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.matching_manager.databinding.MatchFragmentBinding
 
 class MatchFragment : Fragment() {
     private var _binding: MatchFragmentBinding? = null
     private val binding get() = _binding!!
-
-//    private val viewModel: MatchViewModel by lazy {
-//        ViewModelProvider(this)[MatchViewModel::class.java]
-//    }
 
     private val viewModel : MatchViewModel by viewModels {
         MatchViewModelFactory()
@@ -33,9 +28,15 @@ class MatchFragment : Fragment() {
     companion object {
         fun newInstance() = MatchFragment()
         const val OBJECT_DATA = "item_object"
+        const val ID_DATA = "item_userId"
         fun newIntent(context: Context, item: MatchDataModel): Intent {
             val intent = Intent(context, MatchDetailActivity::class.java)
             intent.putExtra(OBJECT_DATA, item)
+            return intent
+        }
+        fun secondIntent(context: Context, userId : String): Intent {
+            val intent = Intent(context, MatchWritingActivity::class.java)
+            intent.putExtra(ID_DATA, userId)
             return intent
         }
     }
@@ -57,15 +58,22 @@ class MatchFragment : Fragment() {
 
     private fun initView() = with(binding) {
 
+        viewModel.updateList()
+
         rv.adapter = adapter
 
         rv.layoutManager = LinearLayoutManager(requireContext())
+
 
         btnCategory.setOnClickListener {
             val matchCategory = MatchCategory()
 
             val fragmentManager = requireActivity().supportFragmentManager
             matchCategory.show(fragmentManager, matchCategory.tag)
+        }
+
+        fabAdd.setOnClickListener {
+            startActivity(secondIntent(requireContext(), "Test"))
         }
     }
 
