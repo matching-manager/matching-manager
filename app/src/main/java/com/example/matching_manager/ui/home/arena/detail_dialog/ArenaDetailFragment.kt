351eeff0f112187a4d7268fe049ef8db2616e06b
추민수 <devsoftychoo@gmail.com>
2023-10-16 17:07:41 +0900

    [Feat] : 전화걸기, URL 공유

--- app/src/main/java/com/example/matching_manager/ui/home/arena/detail_dialog/ArenaDetailFragment.kt
+++ app/src/main/java/com/example/matching_manager/ui/home/arena/detail_dialog/ArenaDetailFragment.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.matching_manager.databinding.ArenaDetailFragmentBinding
+import com.example.matching_manager.ui.home.arena.ArenaViewModel
+import com.example.matching_manager.util.Utils
 
 class ArenaDetailFragment : DialogFragment() {
 
     private var _binding : ArenaDetailFragmentBinding? = null
 
     private val binding get() = _binding!!
+
+    private val sharedViewModel : ArenaViewModel by activityViewModels()
+
+    private val viewModel : ArenaDetailViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +33,7 @@ class ArenaDetailFragment : DialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = ArenaDetailFragmentBinding.inflate(inflater,container,false)
         return binding.root
     }
@@ -31,18 +41,43 @@ class ArenaDetailFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
+        initViewModels()
+    }
+
+    private fun initViewModels() = with(binding){
+        with(sharedViewModel){
+            item.observe(viewLifecycleOwner, Observer { item ->
+                viewModel.updateItemDetail(item)
+            })
+        }
+        with(viewModel){
+            detailItem.observe(viewLifecycleOwner, Observer {
+                tvTitle.text = it.placeName
+                tvLocation.text = it.addressName
+                tvCall.text = it.phone
+            })
+        }
     }
 
     private fun initView() = with(binding){
-
         btnShare.setOnClickListener {
-
+            Utils.shareUrl(requireContext(),viewModel.getPlaceUrl())
         }
         btnCall.setOnClickListener {
 
+            activity?.let { activity ->
+                viewModel.getPhoneNumber()?.let { phoneNumber ->
+                    Utils.callPhoneNumber(activity, phoneNumber)
+                } ?: run {
+                    Toast.makeText(requireContext(), "전화번호가 없습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
-        btnShare.setOnClickListener {
-
+        btnCancel.setOnClickListener {
+            val fragment = requireActivity().supportFragmentManager.findFragmentByTag("SampleDialog")
+            if (fragment != null) {
+                requireActivity().supportFragmentManager.beginTransaction().remove(fragment).commit()
+            }
         }
     }
 
