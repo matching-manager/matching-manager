3375a6adfcccc56b7a443d50a3d5d659b5f5659d
추민수 <devsoftychoo@gmail.com>
2023-10-18 23:02:21 +0900

    [Feat] : Background상태 알림처리 완료

--- app/src/main/java/com/example/matching_manager/ui/home/alarm/AlarmActivity.kt
+++ app/src/main/java/com/example/matching_manager/ui/home/alarm/AlarmActivity.kt
@@ -1,13 +1,74 @@
 package com.example.matching_manager.ui.home.alarm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.example.matching_manager.databinding.AlarmActivityBinding
+import com.example.matching_manager.fcm.MyFirebaseMessagingService
 
 class AlarmActivity : AppCompatActivity() {
+
+    companion object {
+//        fun newIntent(
+//            context: Context,
+//            phoneNumber:String,
+//            userId:String
+//        ) = Intent(context,AlarmActivity::class.java).apply {
+//            putExtra(MyFirebaseMessagingService.RECEIVED_USER_PHONE_NUMBER, phoneNumber)
+//            putExtra(MyFirebaseMessagingService.RECEIVED_USER_ID, userId)
+//        }
+
+    }
+
+    private val viewModel: AlarmViewModel by viewModels()
+
+    private val listAdapter by lazy {
+        AlarmListAdapter(
+            onClick = { item ->
+
+            }
+        )
+    }
+
     private val binding by lazy { AlarmActivityBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        initView()
+        initViewModel()
     }
+
+    private fun initViewModel() = with(viewModel) {
+        list.observe(this@AlarmActivity, Observer {
+            listAdapter.submitList(it)
+        })
+    }
+
+    private fun initView() = with(binding) {
+        rvAlarm.adapter = listAdapter
+
+        val userId = intent.getStringExtra(MyFirebaseMessagingService.RECEIVED_USER_ID)
+        val userPhoneNumber = intent.getStringExtra(MyFirebaseMessagingService.RECEIVED_USER_PHONE_NUMBER)
+        val body = intent.getStringExtra(MyFirebaseMessagingService.RECEIVED_BODY)
+        Log.d("AlarmActivity","value $userId $userPhoneNumber $body")
+        if(userId !=null){
+            viewModel.addAlarm(userId, body, userPhoneNumber)
+        }
+    }
+
+//    override fun onNewIntent(intent: Intent?) {
+//        super.onNewIntent(intent)
+//        Log.d("AlarmActivity","intent")
+//        intent?.let {
+//            val userId = intent.getStringExtra(MyFirebaseMessagingService.RECEIVED_USER_ID)
+//            val userPhoneNumber = intent.getStringExtra(MyFirebaseMessagingService.RECEIVED_USER_PHONE_NUMBER)
+//            val body = intent.getStringExtra(MyFirebaseMessagingService.RECEIVED_BODY)
+//            Log.d("AlarmActivity","value $userId $userPhoneNumber $body")
+//            viewModel.addAlarm(userId, body, userPhoneNumber)
+//        }
+//    }
 }
--- app/src/main/java/com/example/matching_manager/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/matching_manager/ui/main/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.matching_manager.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.matching_manager.R
 import com.example.matching_manager.databinding.MainActivityBinding
+import com.example.matching_manager.fcm.MyFirebaseMessagingService
+import com.example.matching_manager.ui.home.alarm.AlarmActivity
 import com.google.android.material.tabs.TabLayoutMediator
 
 
@@ -35,5 +39,36 @@ class MainActivity : AppCompatActivity() {
         tabLayout.getTabAt(3)?.setIcon(R.drawable.ic_team_grey)
         tabLayout.getTabAt(4)?.setIcon(R.drawable.ic_my_grey)
 
+        // background -> Intent -> Alarm
+        intent?.let {
+            Log.d("MainActivity","${intent.extras}")
+            val userId = intent.extras?.getString("user_id")
+            val phoneNumber = intent.extras?.getString("phone_number")
+            val body = intent.extras?.getString("body")
+            Log.d("MainActivity","value : $userId, $phoneNumber $body")
+
+            val intent = Intent(this@MainActivity,AlarmActivity::class.java)
+            intent.apply {
+                intent.putExtra(MyFirebaseMessagingService.RECEIVED_USER_ID,userId)
+                intent.putExtra(MyFirebaseMessagingService.RECEIVED_USER_PHONE_NUMBER,phoneNumber)
+                intent.putExtra(MyFirebaseMessagingService.RECEIVED_BODY,phoneNumber)
+            }
+            if (userId != null){
+                startActivity(intent)
+            }
+        }
     }
+
+//    override fun onNewIntent(intent: Intent?) {
+//        super.onNewIntent(intent)
+//        Log.d("MainActivity","intent")
+//        intent?.let {
+//            Log.d("MainActivity","${intent.extras}")
+//
+//            val userId = intent.getStringExtra(MyFirebaseMessagingService.RECEIVED_USER_ID)
+//            val userPhoneNumber = intent.getStringExtra(MyFirebaseMessagingService.RECEIVED_USER_PHONE_NUMBER)
+//            val body = intent.getStringExtra(MyFirebaseMessagingService.RECEIVED_BODY)
+//            Log.d("MainActivity","value $userId $userPhoneNumber $body")
+//        }
+//    }
 }
