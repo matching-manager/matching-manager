6deff073b43223a9a71d1e2c606e2073c5e17f8a
hjdevelop <hyunjun6133@naver.com>
2023-10-18 17:05:24 +0900

    [FEAT] : 임시 데이터 생성

--- app/src/main/java/com/example/matching_manager/ui/match/MatchDataModel.kt
+++ app/src/main/java/com/example/matching_manager/ui/match/MatchDataModel.kt
@@ -1,22 +1,23 @@
 package com.example.matching_manager.ui.match
 
 import android.os.Parcelable
+import com.example.matching_manager.R
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class MatchDataModel(
-    val matchId : Int,
-    val userId : String,    //유저ID
-    var userImg: Int,     //유저이미지
-    var teamName : String,  //팀이름
-    var matchType : String, //경기종류 (축구, 풋살, 농구, 탁구, 볼링)
-    var dateTime : String,  //경기시간
-    var matchPlace: String, //경기장소
-    var playerNum: Int,  //경기인원
-    var entryFee: Int,   //참가비
-    var description : String,   //정보
-    var gender : String,    //성별
-    var postImg : Int,      //게시글 사진
-    var viewCount: Int,     //조회수
-    var applyCount: Int,  //신청수
+    val matchId : Int = 20,
+    val userId : String = "userId",    //유저ID
+    var userImg: Int = 0,     //유저이미지
+    var teamName : String = "team",  //팀이름
+    var matchType : String = "type", //경기종류 (축구, 풋살, 농구, 탁구, 볼링)
+    var dateTime : String = "10월 20일 오후 8시",  //경기시간
+    var matchPlace: String = "경기도 안양시 평촌 중앙공원 축구장", //경기장소
+    var playerNum: Int = 11,  //경기인원
+    var entryFee: Int = 10000,   //참가비
+    var description : String = "정보",   //정보
+    var gender : String = "남성",    //성별
+    var postImg : Int = 0,      //게시글 사진
+    var viewCount: Int = 5,     //조회수
+    var applyCount: Int = 1,  //신청수
 ) : Parcelable
