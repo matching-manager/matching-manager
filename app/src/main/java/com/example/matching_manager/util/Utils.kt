9b90b31baa1dec1cc7eaae668916c22c6a986edf
추민수 <devsoftychoo@gmail.com>
2023-10-25 17:40:39 +0900

    [Feat] : GeoRepository
    1. Util에 있던 경도 위도 받아오는 Geocoding을 데이터 Layer로 이동
    2. Repository facade pattern 구성

--- app/src/main/java/com/example/matching_manager/util/Utils.kt
+++ app/src/main/java/com/example/matching_manager/util/Utils.kt
@@ -20,26 +20,6 @@ object Utils {
 
     private const val REQUEST_PHONE_CALL = 1
 
-    // 주소로 위도,경도 구하는 GeoCoding
-    // getFromLocationName()을 이용해 Location 객체로 위도 경도를 return하는 함수
-    // 인자로 장소( string )와 maxResult( 결과의 최대 개수 int )로 구성, 따라서 사용자가 입력한 주소 str과 maxResult(int)를 인자로 넣어준다.
-    fun geoCoding(address: String, context : Context): Location {
-        return try {
-            Geocoder(context, Locale.KOREA).getFromLocationName(address, 1)?.let{
-                Location("").apply {
-                    latitude =  it[0].latitude
-                    longitude = it[0].longitude
-                }
-            }?: Location("").apply {
-                latitude = 0.0
-                longitude = 0.0
-            }
-        }catch (e:Exception) {
-            e.printStackTrace()
-            geoCoding(address,context) //재시도
-        }
-    }
-
     fun shareUrl(context: Context, url : String){
         val share = Intent.createChooser(Intent().apply {
             action = Intent.ACTION_SEND
