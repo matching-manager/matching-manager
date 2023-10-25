9abccb5ac1b3579103dd4f37ed8de1d12e7254df
추민수 <devsoftychoo@gmail.com>
2023-10-25 11:32:26 +0900

    [Refactoring] : Send Fcm
    1. Activity -> DialogFragment로 변경
    2. ID, Pw값을 같이 전송할 수 있도록 구현 완료

---
+++ app/src/main/java/com/example/matching_manager/fcm/send/SendFcmFragment.kt
@@ -0,0 +1,89 @@
+package com.example.matching_manager.fcm.send
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import com.example.matching_manager.R
+import com.example.matching_manager.databinding.SendFcmFragmentBinding
+import com.example.matching_manager.retrofit.fcm.FcmRetrofitClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+
+class SendFcmFragment : DialogFragment() {
+
+    private var _binding :SendFcmFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        const val TAG = "SendFragment"
+    }
+    private val viewModel: SendFcmViewModel by viewModels { SendFcmViewModelFactory() }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = SendFcmFragmentBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() = with(binding){
+
+        // Dialog의 배경을 투명으로 설정
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        btnSubmit.setOnClickListener {
+            runCatching {
+                if (etId.text.isEmpty()||etPhoneNumber.text.isEmpty()){
+                    Toast.makeText(context,"빈칸을 모두 채워주세요",Toast.LENGTH_SHORT).show()
+                }else{
+                    val fcmData = Payload(
+                        "c72G5pn1RWmv76uCpW7so3:APA91bERbSy1FBaAjIje9p_eBwG8zKd5ZxENue4zQp2lRanmqy5VFJ0Iw3yepVJ7qhZVcFKCApo4kUu8IRcqi4DIC0yIJUmZVVI5mHvIAHws7XWXgfYBqk-GNOhgD7hym0CTLxL0HNoe",
+                        id = etId.text.toString(),
+                        number = etPhoneNumber.text.toString()
+                    )
+                    pushNotification(fcmData)
+                    onDestroyView()
+                }
+            }.onFailure { e ->
+                Log.e(TAG, "Error: ${e.message}")
+            }
+
+        }
+
+        btnCancel.setOnClickListener {
+            onDestroyView()
+        }
+
+    }
+
+    private fun pushNotification(payload: Payload) = CoroutineScope(Dispatchers.IO).launch {
+//        FcmRetrofitClient.api.postNotification(
+//            payload = payload
+//        )
+        viewModel.sendData(payload)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
