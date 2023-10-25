0f65e41a74815766d188af342371175134a7d60d
hjdevelop <hyunjun6133@naver.com>
2023-10-26 06:18:11 +0900

    [Fix] : Match, My ViewModel 리팩토링

--- app/src/main/java/com/example/matching_manager/ui/my/MyMatchRepository.kt
+++ app/src/main/java/com/example/matching_manager/ui/my/MyMatchRepository.kt
@@ -1,9 +1,11 @@
 package com.example.matching_manager.ui.my
 
+import com.google.firebase.database.FirebaseDatabase
+
 interface MyMatchRepository {
-    suspend fun getList(userId : String): List<MyMatchDataModel>
+    suspend fun getList(userId : String, database: FirebaseDatabase): List<MyMatchDataModel>
 
-    suspend fun deleteData(data : MyMatchDataModel)
+    suspend fun deleteData(data : MyMatchDataModel, database: FirebaseDatabase)
 
-    suspend fun editData(data : MyMatchDataModel, newData: MyMatchDataModel)
+    suspend fun editData(data : MyMatchDataModel, newData: MyMatchDataModel, database: FirebaseDatabase)
 }
