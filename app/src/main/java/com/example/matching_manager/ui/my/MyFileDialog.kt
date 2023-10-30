530a9c512b9de371413d669e1796cb51affd24f1
mwkimm <94061061+mwkimm@users.noreply.github.com>
2023-10-30 21:07:53 +0900

    dialog_edit string 추가수정

--- app/src/main/java/com/example/matching_manager/ui/my/MyFileDialog.kt
+++ app/src/main/java/com/example/matching_manager/ui/my/MyFileDialog.kt
@@ -2,33 +2,42 @@ package com.example.matching_manager.ui.my
 
 import android.Manifest
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
 import com.example.matching_manager.databinding.DialogEditBinding
 
 class MyFileDialog(
     private val editName: String?,
     private val editImageUri: Uri?,
-    private val okCallback: (String, Uri?) -> Unit,
+    val okCallback: (String, String, String, Uri?) -> Unit,
 
 
     ) : DialogFragment() {
 
     private lateinit var binding: DialogEditBinding
     private var selectedImageUri: Uri? = null
+
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    private val viewModel: MyViewModel by viewModels {
+        MyMatchViewModelFactory()
+    }
 
     companion object {
         const val PICK_IMAGE_REQUEST_CODE = 123
@@ -44,50 +53,78 @@ class MyFileDialog(
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+//        resultLauncher =
+//            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+//                if (result.resultCode == Activity.RESULT_OK) {
+//                    viewModel.fetchData(viewModel.userId)
+//
+//                    val data: Intent? = result.data
+//                    selectedImageUri = data?.data
+//                    if (selectedImageUri != null) {
+//                        // 이미지를 선택한 후의 로직을 수행합니다.
+//                        binding.ivProfile.setImageURI(selectedImageUri)
+//
+//                        //ivMypageFace.setImageURI(selectedImageUri)
+//                        //setProfileImage(selectedImageUri)
+//                        Log.d(
+//                            "MyFragment",
+//                            "After save click: selectedImageUri = $selectedImageUri"
+//                        )
+//                    }
+//                }
+//            }
+
         initView()
+
+
     }
 
     private fun initView() = with(binding) {
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+
 
         btnSave.setOnClickListener {
             if (etContentNickname.text.isNullOrBlank()) {
                 Toast.makeText(context, "닉네임 입력하십시요!", Toast.LENGTH_SHORT).show()
             } else {
                 val newName = etContentNickname.text.toString()
+                val newLocation = etContentLocation.text.toString()
+                val newId = etContentId.text.toString()
+
                 val newImageUri = selectedImageUri
 
                 // 데이터 SharedPreferences에 저장
-                saveProfiledData(newName, newImageUri)
-                okCallback(etContentNickname.text.toString(), selectedImageUri)
+                //saveProfiledData(newName, newImageUri)
+                okCallback(
+                    newName,
+                    newLocation,
+                    newId,
+                    newImageUri
+                )
                 dismiss()
             }
         }
         btnCancle.setOnClickListener {
             dismiss()
         }
+
         ivProfile.setOnClickListener {
-            checkPermissionGalleryAndNavigateGallery()
+            //checkPermissionGalleryAndNavigateGallery()
+            openGallery(resultLauncher)
+
         }
         editName?.let { etContentNickname.setText(it) }
         editImageUri?.let {
             selectedImageUri = it
             ivProfile.setImageURI(it)
         }
-    }
 
-    private fun saveProfiledData(name: String, imageUri: Uri?) {
-        val sharedPreferences =
-            context?.getSharedPreferences("MyProfilePreferences", Context.MODE_PRIVATE)
-        val editor = sharedPreferences?.edit()
 
-        editor?.putString("profileName", name)
-        editor?.putString("profileImageUri", imageUri?.toString())
-
-        editor?.apply()
     }
 
     private fun checkPermissionGalleryAndNavigateGallery() {
@@ -109,43 +146,62 @@ class MyFileDialog(
 
             else -> {
                 requestPermissions(
-                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                PERMISSION_REQUEST_CODE
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSION_REQUEST_CODE
                 )
 
             }
         }
     }
 
-    private fun showPermissionContextPopup(){
+    private fun showPermissionContextPopup() {
         AlertDialog.Builder(requireContext())
             .setTitle("권한 필요")
             .setMessage("프로필 사진을 변경하려면 저장소 읽기 권한이 필요합니다.")
             .setPositiveButton("동의") { _, _ ->
                 requestPermissions(
                     arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                PERMISSION_REQUEST_CODE
+                    PERMISSION_REQUEST_CODE
                 )
             }
+            .create()
+            .show()
     }
 
-    private fun navigateGallery(){
+    private fun openGallery(resultLauncher: ActivityResultLauncher<Intent>) {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        this.resultLauncher.launch(intent)
+        //startActivityForResult(intent, MyFragment.PICK_IMAGE_REQUEST)
+    }
+
+    private fun navigateGallery() {
         val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
         intent.type = "image/*"
-
         startActivityForResult(intent, PICK_IMAGE_REQUEST_CODE)
     }
+
+//    fun openGallery() {
+//        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+//        startActivityForResult(intent, MyFragment.PICK_IMAGE_REQUEST)
+//    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == PICK_IMAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             val imageUri: Uri? = data?.data
-            imageUri?.let { activity?.contentResolver?.takePersistableUriPermission(it, Intent.FLAG_GRANT_READ_URI_PERMISSION) }
+            imageUri?.let {
+                activity?.contentResolver?.takePersistableUriPermission(
+                    it,
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION
+                )
+            }
             selectedImageUri = imageUri
             binding.ivProfile.setImageURI(imageUri)
         }
     }
+
+
 }
 
 
--- app/src/main/java/com/example/matching_manager/ui/my/MyFragment.kt
+++ app/src/main/java/com/example/matching_manager/ui/my/MyFragment.kt
@@ -10,31 +10,29 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.matching_manager.R
 import com.example.matching_manager.databinding.DialogEditBinding
 import com.example.matching_manager.databinding.MyFragmentBinding
-import kotlinx.coroutines.launch
 
 class MyFragment : Fragment() {
     private var _binding: MyFragmentBinding? = null
     private val binding get() = _binding!!
-
     private val viewModel: MyViewModel by viewModels {
         MyMatchViewModelFactory()
     }
 
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    private var editName: String? = null // editName 선언
+    private var editImageUri: Uri? = null // editImageUri 선언
 
     private val adapter by lazy {
         MyMatchListAdapter(
@@ -96,11 +94,26 @@ class MyFragment : Fragment() {
         return binding.root
     }
 
+    private lateinit var myFileDialog: MyFileDialog
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initView()
         initViewModel()
+
+        myFileDialog = MyFileDialog(editName, editImageUri) { newName, newLocation, newId, newImageUri ->
+            // newName, newLocation, newId, newImageUri를 이곳에서 사용할 수 있습니다.
+        }
+
+
+        setFragmentResultListener("imageResult") { _, result ->
+            val selectedImageUri = result.getParcelable<Uri>("selectedImageUri")
+            if (selectedImageUri != null) {
+                //dialogBinding.ivProfile.setImageURI(selectedImageUri) // 다이얼로그 내에서의 이미지 업데이트
+                //binding.ivMypageFace.setImageURI(selectedImageUri) // ivMypageFace 이미지 업데이트
+                Log.d("myFragment1", "After save click: selectedImageUri = $selectedImageUri")
+            }
+        }
     }
 
 
@@ -114,10 +127,25 @@ class MyFragment : Fragment() {
         manager.stackFromEnd = true
         rv.layoutManager = manager
 
+
         resultLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
                 if (result.resultCode == Activity.RESULT_OK) {
                     viewModel.fetchData(viewModel.userId)
+
+                    val data: Intent? = result.data
+                    selectedImageUri = data?.data
+                    if (selectedImageUri != null) {
+                        // 이미지를 선택한 후의 로직을 수행합니다.
+                        dialogBinding.ivProfile.setImageURI(selectedImageUri)
+
+                        //ivMypageFace.setImageURI(selectedImageUri)
+                        //setProfileImage(selectedImageUri)
+                        Log.d(
+                            "MyFragment",
+                            "After save click: selectedImageUri = $selectedImageUri"
+                        )
+                    }
                 }
             }
 
@@ -130,87 +158,105 @@ class MyFragment : Fragment() {
             val dialog = builder.create()
             dialog.show()
 
-            val et_content_nickname = dialogView.findViewById<EditText>(R.id.et_content_nickname)
-            val et_content_location = dialogView.findViewById<EditText>(R.id.et_content_location)
-            val et_content_id = dialogView.findViewById<EditText>(R.id.et_content_id)
-            val btn_save = dialogView.findViewById<Button>(R.id.btn_save)
-            val btn_cancle = dialogView.findViewById<Button>(R.id.btn_cancle)
-            val iv_profile = dialogView.findViewById<ImageView>(R.id.iv_profile)
+            //val et_content_nickname = dialogView.findViewById<EditText>(R.id.et_content_nickname)
+            //val et_content_location = dialogView.findViewById<EditText>(R.id.et_content_location)
+            //val et_content_id = dialogView.findViewById<EditText>(R.id.et_content_id)
+            //val btn_save = dialogView.findViewById<Button>(R.id.btn_save)
+            //val btn_cancle = dialogView.findViewById<Button>(R.id.btn_cancle)
+            //val iv_profile = dialogView.findViewById<ImageView>(R.id.iv_profile)
+
+            val et_content_nickname = dialogBinding.etContentNickname
+            val et_content_location = dialogBinding.etContentLocation
+            val et_content_id = dialogBinding.etContentId
+            val btn_save = dialogBinding.btnSave
+            val btn_cancle = dialogBinding.btnCancle
+            val iv_profile = dialogBinding.ivProfile
 
             btn_save.setOnClickListener {
+                ivMypageFace.setImageURI(selectedImageUri)
                 with(binding) {
                     val nickname = et_content_nickname.text.toString()
                     val location = et_content_location.text.toString()
                     val id = et_content_id.text.toString()
 
-                    selectedImageUri?.let { imageUri ->
-                        ivMypageFace.setImageURI(imageUri)
+                    //setProfileImage(selectedImageUri)
+
+                    if (nickname.isNullOrBlank() || location.isNullOrBlank() || id.isNullOrBlank()) {
+                        // btn_save.isEnabled
+                        Toast.makeText(
+                            requireContext(),
+                            "닉네임, 위치, 아이디를 입력해주세요.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                    } else {
+
+                        btnMypageNickname.text = nickname
+                        btnMypageLocation.text = location
+                        btnMypageId.text = id
+
+                        //val newName = et_content_nickname.text.toString()
+                        val newImageUri = selectedImageUri
+                        dialog.dismiss()
                     }
-
-                    btnMypageNickname.text = nickname
-                    btnMypageLocation.text = location
-                    btnMypageId.text = id
-
-                    dialog.dismiss()
                 }
-                //setProfileImage(selectedImageUri)
-
-
-//                if (et_content_nickname.text.isNullOrBlank() && et_content_location.text.isNullOrBlank() && et_content_id.text.isNullOrBlank()) {
-//                    Toast.makeText(context, "입력하십시요!", Toast.LENGTH_SHORT).show()
-//                }  else {
-//                    val newName = et_content_nickname.text.toString()
-//                    val newImageUri = selectedImageUri
-//
-//                    // 데이터 SharedPreferences에 저장
-//                    //saveProfiledData(newName, newImageUri)
-//                    //okCallback(etContent.text.toString(), selectedImageUri)
-//                }
 
             }
 
+
             btn_cancle.setOnClickListener {
+                //selectedImageUri = null
+                dialogBinding.ivProfile.setImageURI(null)
+
                 dialog.dismiss()
             }
 
-//            btnEdit.setOnClickListener{
-//                openGallery()
-//            }
 
             iv_profile.setOnClickListener {
+
+                //myFileDialog.openGallery(resultLauncher)
                 openGallery()
+
             }
         }
-    }
+
+
+        }
 
     private fun openGallery() {
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+        resultLauncher.launch(intent)
+        //        startActivityForResult(intent, MyFragment.PICK_IMAGE_REQUEST)
     }
 
     private fun setProfileImage(imageUri: Uri?) {
-        dialogBinding.ivProfile.setImageURI(imageUri)
-        binding.ivMypageFace.setImageURI(imageUri)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
-            val selectedImageUri = data.data
-            //dialogBinding.ivProfile.setImageURI(selectedImageUri)
-            //binding.ivMypageFace.setImageURI(selectedImageUri)
-            setProfileImage(selectedImageUri)
+        val result = Bundle().apply {
+            putParcelable("selectedImageUri", imageUri)
         }
+        setFragmentResult("imageResult", result)
     }
 
-    private fun showProfileDialog() = with(binding) {
-        //MyFileDialog(
-        //viewModel.profileName.value,
-        //viewModel.profileImageUri.value
-        //) { newName, newImageUri ->
-        //viewModel.setProfile(newName, newImageUri)
-        //}.show(parentFragmentManager, "MyFileDialog")
+    private fun saveProfiledData(name: String, imageUri: Uri?) {
+        val sharedPreferences =
+            context?.getSharedPreferences("MyProfilePreferences", Context.MODE_PRIVATE)
+        val editor = sharedPreferences?.edit()
+
+        editor?.putString("profileName", name)
+        editor?.putString("profileImageUri", imageUri?.toString())
+
+        editor?.apply()
     }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+//            val selectedImageUri = data.data
+//            //dialogBinding.ivProfile.setImageURI(selectedImageUri)
+//            //binding.ivMypageFace.setImageURI(selectedImageUri)
+//            setProfileImage(selectedImageUri)
+//        }
+//    }
+
 
     private fun initViewModel() = with(viewModel) {
         list.observe(viewLifecycleOwner, Observer {
