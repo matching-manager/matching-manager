e9fea7e67bdeb8c10b13b752a35d30320e95f099
추민수 <devsoftychoo@gmail.com>
2023-10-31 19:34:24 +0900

    [Feat] : UserInfoModel
    user에 대한 정보를 관리하고 넘겨주기 쉽게 만들어주는 데이터클래스 구현

---
+++ app/src/main/java/com/example/matching_manager/data/model/UserInfoModel.kt
@@ -0,0 +1,9 @@
+package com.example.matching_manager.data.model
+
+data class UserInfoModel(
+    val uid : String?,
+    val uidToken : String?,
+    val email:String?,
+    val fcmToken : String?,
+    val photoUrl : String?
+)
