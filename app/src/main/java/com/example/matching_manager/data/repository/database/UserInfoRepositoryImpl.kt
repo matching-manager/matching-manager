aa56f3635d4edc388f1df1c54ea13ec7454ab175
추민수 <devsoftychoo@gmail.com>
2023-10-31 19:31:49 +0900

    [Feat] : UserInfoRepositoryImpl
    유저 정보를 데이터베이스에 저장할 Repository 구현부 클래스 구현

---
+++ app/src/main/java/com/example/matching_manager/data/repository/database/UserInfoRepositoryImpl.kt
@@ -0,0 +1,47 @@
+package com.example.matching_manager.data.repository.database
+
+import com.example.matching_manager.data.model.UserInfoModel
+import com.example.matching_manager.domain.repository.database.UserInfoRepository
+import com.example.matching_manager.ui.match.MatchDataModel
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.coroutines.tasks.await
+
+class UserInfoRepositoryImpl : UserInfoRepository {
+
+
+    override suspend fun adduser(data: UserInfoModel, database: FirebaseDatabase) {
+        val userRef = database.getReference("User")
+        userRef.push().setValue(data).await()
+    }
+}
+//override suspend fun adduser(data: UserInfoModel, database: FirebaseDatabase) {
+//    val userRef = database.getReference("User")
+//
+//    // 중복 정보를 확인할 쿼리 생성
+//    val query = userRef.orderByChild("uid").equalTo(data.uid)
+//
+//    // 쿼리 실행
+//    val snapshot = query.get().await()
+//
+//    if (snapshot.exists()) {
+//        // 중복 정보가 이미 데이터베이스에 있는 경우, 업데이트 수행
+//        for (childSnapshot in snapshot.children) {
+//            val key = childSnapshot.key
+//            key?.let {
+//                val updateData = mapOf(
+//                    "uid" to data.uid,
+//                    "uidToken" to data.uidToken,
+//                    "email" to data.email,
+//                    "fcmToken" to data.fcmToken,
+//                    "photoUrl" to data.photoUrl
+//                )
+//
+//                // 중복된 데이터 업데이트
+//                userRef.child(key).updateChildren(updateData).await()
+//            }
+//        }
+//    } else {
+//        // 중복 정보가 없는 경우, 데이터를 저장
+//        userRef.push().setValue(data).await()
+//    }
+//}
