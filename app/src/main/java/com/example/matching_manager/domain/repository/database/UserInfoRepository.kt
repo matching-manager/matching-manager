90bbebd302fd9489aa56af4e81d275c4e8ee3063
추민수 <devsoftychoo@gmail.com>
2023-10-31 19:32:36 +0900

    [Feat] : Facade Repository
    view단에 단순한 인터페이스를 저장할 Repository interface (Facade 패턴 적용)

---
+++ app/src/main/java/com/example/matching_manager/domain/repository/database/UserInfoRepository.kt
@@ -0,0 +1,9 @@
+package com.example.matching_manager.domain.repository.database
+
+import com.example.matching_manager.data.model.UserInfoModel
+import com.google.firebase.database.FirebaseDatabase
+
+interface UserInfoRepository {
+
+    suspend fun adduser(data: UserInfoModel, database: FirebaseDatabase)
+}
