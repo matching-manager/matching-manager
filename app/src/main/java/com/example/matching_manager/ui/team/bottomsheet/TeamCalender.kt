971c1cf47b169f3e080b5b64df6780b2be9939d6
yeonghyeon <agilecatch@gmail.com>
2023-11-03 01:04:26 +0900

    [Feat] : 글쓰기 캘린더에서 이전 날짜는 선택할 수 없도록 예외처리
    - 글쓰기 부분에서 과거의 날짜를 선택할 수 없도록 하였습니다.
    - 날짜를 선택하지 않고 확인을 누르면 현재 날짜가 추가되도록 하였습니다.

--- app/src/main/java/com/example/matching_manager/ui/team/bottomsheet/TeamCalender.kt
+++ app/src/main/java/com/example/matching_manager/ui/team/bottomsheet/TeamCalender.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import com.example.matching_manager.databinding.TeamCalenderBinding
 import com.example.matching_manager.ui.team.viewmodel.TeamSharedViewModel
@@ -39,7 +40,19 @@ class TeamCalender : BottomSheetDialogFragment() {
     private fun initView() = with(binding) {
         // 캘린더 날짜가 선택될 때의 리스너를 등록합니다.
         materialCalendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
+
             val calendar = Calendar.getInstance()
+            val today = Calendar.getInstance()
+
+            calendar.set(year, month, dayOfMonth) // 선택된 날짜로 설정
+
+            // 만약 선택한 날짜가 오늘보다 이전인 경우
+            if (calendar.before(today)) {
+                // 토스트 메시지로 안내를 띄웁니다.
+                Toast.makeText(requireContext(), "과거의 날짜는 선택할 수 없습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnDateChangeListener // 날짜 선택을 무시하고 함수를 종료합니다.
+            }
+
             val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
 
             val dayOfWeekString = when (dayOfWeek) {
@@ -53,12 +66,30 @@ class TeamCalender : BottomSheetDialogFragment() {
                 else -> ""
             }
             btnSelect.setOnClickListener {
-                sharedViewModel.updateCalendar(year, month+ 1, dayOfMonth, dayOfWeekString)
+                sharedViewModel.updateCalendar(year, month + 1, dayOfMonth, dayOfWeekString)
                 dismiss() // BottomSheet 닫기
             }
-
         }
+        btnSelect.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH) + 1
+            val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+            val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
 
+            val dayOfWeekString = when (dayOfWeek) {
+                Calendar.SUNDAY -> "(일)"
+                Calendar.MONDAY -> "(월)"
+                Calendar.TUESDAY -> "(화)"
+                Calendar.WEDNESDAY -> "(수)"
+                Calendar.THURSDAY -> "(목)"
+                Calendar.FRIDAY -> "(금)"
+                Calendar.SATURDAY -> "(토)"
+                else -> "알 수 없음"
+            }
+            sharedViewModel.updateCalendar(year, month, dayOfMonth, dayOfWeekString)
+            dismiss() // BottomSheet 닫기
+        }
         btnCancel.setOnClickListener {
             dismiss()
         }
