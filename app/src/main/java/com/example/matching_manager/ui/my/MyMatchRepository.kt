482d7b5a7843417ab81f315158280d19760fb3bd
hjdevelop <hyunjun6133@naver.com>
2023-11-03 07:50:29 +0900

    [Fix] : My Page에 나의 Team 데이터도 CRUD하도록 기능 추가

--- app/src/main/java/com/example/matching_manager/ui/my/MyMatchRepository.kt
+++ app/src/main/java/com/example/matching_manager/ui/my/MyMatchRepository.kt
@@ -1,11 +1,24 @@
 package com.example.matching_manager.ui.my
 
+import com.example.matching_manager.ui.team.TeamItem
 import com.google.firebase.database.FirebaseDatabase
 
 interface MyMatchRepository {
-    suspend fun getList(userId : String, database: FirebaseDatabase): List<MyMatchDataModel>
+    suspend fun getMatchList(userId : String, database: FirebaseDatabase): List<MyMatchDataModel>
 
-    suspend fun deleteData(data : MyMatchDataModel, database: FirebaseDatabase)
+    suspend fun deleteMatchData(data : MyMatchDataModel, database: FirebaseDatabase)
 
-    suspend fun editData(data : MyMatchDataModel, newData: MyMatchDataModel, database: FirebaseDatabase)
+    suspend fun editMatchData(data : MyMatchDataModel, newData: MyMatchDataModel, database: FirebaseDatabase)
+
+    suspend fun getRecruitList(userId : String, database: FirebaseDatabase): List<TeamItem.RecruitmentItem>
+
+    suspend fun deleteRecruitData(data : TeamItem.RecruitmentItem, database: FirebaseDatabase)
+
+    suspend fun editRecruitData(data : TeamItem.RecruitmentItem, newData: TeamItem.RecruitmentItem, database: FirebaseDatabase)
+
+    suspend fun getApplicationList(userId : String, database: FirebaseDatabase): List<TeamItem.ApplicationItem>
+
+    suspend fun deleteApplicationData(data : TeamItem.ApplicationItem, database: FirebaseDatabase)
+
+    suspend fun editApplicationData(data : TeamItem.ApplicationItem, newData: TeamItem.ApplicationItem, database: FirebaseDatabase)
 }
