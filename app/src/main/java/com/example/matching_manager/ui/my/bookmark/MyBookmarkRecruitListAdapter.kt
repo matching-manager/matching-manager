c90392ee16b0d5311c76c918574050aa1ef85575
hjdevelop <hyunjun6133@naver.com>
2023-11-06 20:06:41 +0900

    [Feat] : 관심 목록 내 매치, 용병모집, 용병신청 리스트어댑터 생성

---
+++ app/src/main/java/com/example/matching_manager/ui/my/bookmark/MyBookmarkRecruitListAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.matching_manager.ui.my.bookmark
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.matching_manager.databinding.MyRecruitItemBinding
+import com.example.matching_manager.ui.team.TeamItem
+
+class MyBookmarkRecruitListAdapter (private val onItemClick: (TeamItem.RecruitmentItem) -> Unit,
+                                private val onMenuClick : (TeamItem.RecruitmentItem) -> Unit) : ListAdapter<TeamItem.RecruitmentItem, MyBookmarkRecruitListAdapter.ViewHolder>(
+    object : DiffUtil.ItemCallback<TeamItem.RecruitmentItem>() {
+        override fun areItemsTheSame(oldItem: TeamItem.RecruitmentItem, newItem: TeamItem.RecruitmentItem): Boolean {
+            return oldItem.teamId == newItem.teamId
+        }
+
+        override fun areContentsTheSame(oldItem: TeamItem.RecruitmentItem, newItem: TeamItem.RecruitmentItem): Boolean {
+            return oldItem == newItem
+        }
+    }
+){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            MyRecruitItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item, onItemClick, onMenuClick)
+    }
+
+    class ViewHolder(private val binding: MyRecruitItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item : TeamItem.RecruitmentItem, onItemClick: (TeamItem.RecruitmentItem) -> Unit, onMenuClick : (TeamItem.RecruitmentItem) -> Unit) = with(binding) {
+            ivProfile.setImageResource(item.userImg)
+            tvType.text = item.type
+            tvDetail.text = "${item.gender} ${item.playerNum}"
+            tvSchedule.text = item.schedule
+            tvPlace.text = item.area
+
+            btnMenu.setOnClickListener {
+                onMenuClick(item)
+            }
+
+            itemView.setOnClickListener {
+                onItemClick(item)
+            }
+        }
+    }
+}
