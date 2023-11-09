3de91acdc81ca45bcae659021b680ed2d2086568
hjdevelop <hyunjun6133@naver.com>
2023-11-09 21:13:31 +0900

    [Feat] : 홈 화면 매치 게시글 및 공지 게시글 리스트 기능 추가

--- app/src/main/java/com/link_up/matching_manager/ui/home/home/HomeViewModel.kt
+++ app/src/main/java/com/link_up/matching_manager/ui/home/home/HomeViewModel.kt
@@ -3,22 +3,75 @@ package com.link_up.matching_manager.ui.home.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.link_up.matching_manager.ui.match.MatchDataModel
 
 class HomeViewModel : ViewModel() {
     // 공지사항 리스트
-    // TODO : MatchDataModel을 새로운 모델 만들어서 바꾸기
-    private val _announcementList : MutableLiveData<List<MatchDataModel>> = MutableLiveData()
-    val announcementList : LiveData<List<MatchDataModel>> get() = _announcementList
+    private val _announcementList : MutableLiveData<List<AnnouncementDataModel>> = MutableLiveData()
+    val announcementList : LiveData<List<AnnouncementDataModel>> get() = _announcementList
 
-    // 공지사항 모델 -> HomeFragment에서 공지사항 아이템을 클릭하면 바뀌는 모델로 AnnouncementFragment에서
-    // TODO : MatchDataModel을 새로운 모델 만들어서 바꾸기
-    private val _announceModel : MutableLiveData<MatchDataModel> = MutableLiveData()
-    val announceModel : LiveData<MatchDataModel> get() = _announceModel
-
+    private val _announceModel : MutableLiveData<AnnouncementDataModel> = MutableLiveData()
+    val announceModel : LiveData<AnnouncementDataModel> get() = _announceModel
 
     // 추천 경기매칭 리스트
     private val _matchList : MutableLiveData<List<MatchDataModel>> = MutableLiveData()
     val matchList : LiveData<List<MatchDataModel>> get() = _matchList
+
+    private val database =
+        Firebase.database("https://matching-manager-default-rtdb.asia-southeast1.firebasedatabase.app/")
+    private val matchRef = database.getReference("Match")
+    private val announceRef = database.getReference("Announcement")
+
+    fun announcementData(item : AnnouncementDataModel) {
+        _announceModel.value = item
+    }
+
+
+    fun fetchMatchData() {
+        matchRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val dataList = mutableListOf<MatchDataModel>()
+
+                for (childSnapshot in dataSnapshot.children) {
+                    val matchData = childSnapshot.getValue(MatchDataModel::class.java)
+                    if (matchData != null) {
+                        dataList.add(matchData)
+                    }
+                }
+                val currentList = dataList
+                _matchList.value = currentList
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // 오류 처리
+            }
+        })
+    }
+
+    fun fetchAnnounceData() {
+        announceRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val dataList = mutableListOf<AnnouncementDataModel>()
+
+                for (childSnapshot in dataSnapshot.children) {
+                    val announceData = childSnapshot.getValue(AnnouncementDataModel::class.java)
+                    if (announceData != null) {
+                        dataList.add(announceData)
+                    }
+                }
+                val currentList = dataList
+                _announcementList.value = currentList
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // 오류 처리
+            }
+        })
+    }
 
 }
