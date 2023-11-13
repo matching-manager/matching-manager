79cb16369d96adc30a438af3dce656fdc1808152
hjdevelop <hyunjun6133@naver.com>
2023-11-13 20:59:00 +0900

    [Feat] : 임시 커밋

---
+++ app/src/main/java/com/link_up/matching_manager/domain/usecase/team/TeamAutoGetListUseCase.kt
@@ -0,0 +1,7 @@
+package com.link_up.matching_manager.domain.usecase.team
+
+import com.link_up.matching_manager.domain.repository.TeamRepository
+
+class TeamAutoGetListUseCase(val repository: TeamRepository) {
+
+}
